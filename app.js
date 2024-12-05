2befd8003311f3634dffedc4a521c1fae0815fa5
FahimHAssanOvi <fahimhassan125928@gmail.com>
2024-12-05 14:12:58 +0600

    Fuction the available seat and show

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// target
 const selectedSeatEl = document.getElementById('selected-seat');
-console.log('hello');
+const totalBookedEl = document.getElementById('total-booked');
+const availableSeatEl = document.getElementById('available-seat')
+const totalPriceEl = document.getElementById('total-price');
+
+let selectedSeat = [];
+let totalPrice = 0;
+
 function handleSelectSeat(event) {
-    console.log(event.innerText);
-    selectedSeatEl.innerHTML = `
+    // console.log(event.innerText);
+    event.classList.add('bg-primary');
+    event.classList.add('text-white');
+    selectedSeat.push(event.innerText);
+    totalBookedEl.innerText = selectedSeat.length;
+
+    // decrease available seat
+    const availableSeatValue = parseFloat(availableSeatEl.innerText);
+    const newAvailableSeatValue = availableSeatValue - 1;
+    availableSeatEl.innerText = newAvailableSeatValue;
+
+
+    selectedSeatEl.innerHTML += `
     <li class="text-base font-normal flex justify-between">
     <span>${event.innerText}</span>
     <span>Economy</span>
@@ -10,4 +28,9 @@ function handleSelectSeat(event) {
     </li>
 
     `
+    // update total price
+    totalPrice += 550;
+    totalPriceEl.innerText = totalPrice.toFixed(2);
+
+    // console.log(selectedSeat);
 };
